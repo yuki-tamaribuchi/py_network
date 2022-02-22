177382cf7ecff9f9fb2e6d4bfb11a99a6fb3b9cd
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2022-02-22 09:11:46 +0900

    Update ip

--- ip.py
+++ ip.py
@@ -1,9 +1,10 @@
 import struct
 import binascii
+import socket
 
 class IPv4Packet:
-	version_and_ihl = 0x45
-	flag_and_identification = 0x00
+	version_and_ihl = 0b01000101
+	flag_and_flag_offset = 0x00
 	dscp = 0b10000000
 	ttl = 255
 
@@ -20,14 +21,27 @@ class IPv4Packet:
 		self.assemble_ip_fields()
 
 	def assemble_ip_fields(self):
+		src_ip_addr = int("".join([bin(int(x)+256)[3:] for x in self.src.split(".")]),2)
+		dst_ip_addr = int("".join([bin(int(x)+256)[3:] for x in self.dst.split(".")]),2)
+
+		header_sum =  self.version_and_ihl + self.dscp + self.total_length + self.identification + self.flag_and_flag_offset + self.ttl + self.protocol + src_ip_addr + dst_ip_addr
+		check_sum = 0xFFFF - header_sum
+		print(check_sum)
+
+				
+
 		self.raw = struct.pack(
-			"!3h2l2h",
-			self.version_and_ihl,
-			self.dscp,
-			self.total_length,
-			self.identification,
-			self.flag_and_identification,
-			self.ttl,
-			self.protocol,
+			"!2B3h2Bh2L",
+			self.version_and_ihl, #1byte
+			self.dscp, #1byte
+			self.total_length, #2bytes
+			self.identification, #2bytes
+			self.flag_and_flag_offset, #2bytes
+			self.ttl, #1byte
+			self.protocol, #1byte
+			check_sum, #2bytes
+			src_ip_addr, #4bytes
+			dst_ip_addr, #4bytes
 		)
 		print(self.raw)
+		print(len(self.raw))
