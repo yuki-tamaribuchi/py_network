42c3a375da485802af842e3069c39c81e4593eb9
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-24 08:27:30 +0900

    Update main

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from ip import IPv4Packet
 
 if __name__ == "__main__":
 	eth = EthernetFrame()
+	eth_frame = eth.get_ehternet_frame()
 	ip = IPv4Packet(data=b"", src="192.168.11.14")
+	ip_packet = ip.get_ip_packet()
