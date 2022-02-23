8c06911d4248dde64beeb925eab2df589944c09c
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-02-24 08:26:26 +0900

    Update ip

--- ip.py
+++ ip.py
@@ -3,19 +3,20 @@ import binascii
 import socket
 
 class IPv4Packet:
-	version_and_ihl = 0b01000101
+	version_ihl_dscp = 0b0100010110000000
 	flag_and_flag_offset = 0x00
-	dscp = 0b10000000
+	protocol = 0x06
 	ttl = 255
+	ttl_protocol = (ttl<<4) + protocol
 
 
-	def __init__(self, data, src, dst="255.255.255.255", identification=0, protocol=0x06, options=None):
+	def __init__(self, data, src, dst="255.255.255.255"):
 		self.data = data
 		self.src = src
 		self.dst = dst
-		self.identification = identification
-		self.protocol = protocol
-		self.options = options
+		self.identification = 0
+		self.protocol = 0x06
+		self.options = None
 		self.total_length = 0x5 + len(self.data)
 		self.raw = None
 		self.assemble_ip_fields()
@@ -24,24 +25,32 @@ class IPv4Packet:
 		src_ip_addr = int("".join([bin(int(x)+256)[3:] for x in self.src.split(".")]),2)
 		dst_ip_addr = int("".join([bin(int(x)+256)[3:] for x in self.dst.split(".")]),2)
 
-		header_sum =  self.version_and_ihl + self.dscp + self.total_length + self.identification + self.flag_and_flag_offset + self.ttl + self.protocol + src_ip_addr + dst_ip_addr
-		check_sum = 0xFFFF - header_sum
-		print(check_sum)
+		header_sum =  self.version_ihl_dscp 
+		+ self.total_length
+		+ self.identification
+		+ self.flag_and_flag_offset
+		+ self.ttl_protocol
+		+ int(bin(src_ip_addr)[2:18], 2)
+		+ int(bin(src_ip_addr)[18:] ,2)
+		+ int(bin(dst_ip_addr)[2:18], 2)
+		+ int(bin(dst_ip_addr)[18:], 2)
+		
+		
+		check_sum = 0xFF - header_sum
 
 				
 
 		self.raw = struct.pack(
-			"!2B3h2Bh2L",
-			self.version_and_ihl, #1byte
-			self.dscp, #1byte
+			"!6h2L",
+			self.version_ihl_dscp, #2byte
 			self.total_length, #2bytes
 			self.identification, #2bytes
 			self.flag_and_flag_offset, #2bytes
-			self.ttl, #1byte
-			self.protocol, #1byte
+			self.ttl_protocol, #2bytes
 			check_sum, #2bytes
 			src_ip_addr, #4bytes
 			dst_ip_addr, #4bytes
 		)
-		print(self.raw)
-		print(len(self.raw))
+
+	def get_ip_packet(self):
+		return self.raw + self.data
